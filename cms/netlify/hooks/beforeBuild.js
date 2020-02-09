d7d767185fe1562688c309920596b0f58665dd80
JackDownload <kismocsy@gmail.com>
2020-02-09 09:43:04 +0100

    Update beforeBuild.js

--- cms/netlify/hooks/beforeBuild.js
+++ cms/netlify/hooks/beforeBuild.js
@@ -1,9 +1,11 @@
 import generatePostApi from '../build/generate-post-api'
 import generateCategoryApi from '../build/generate-category-api'
 import generateCategoryApi from '../build/generate-page-api
+import generateCategoryApi from '../build/generate-recept-api
 
 export default () => {
   generatePostApi()
   generateCategoryApi()
   generatePageApi()
+  generateReceptApi()
 }
