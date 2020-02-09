b7f4fd35d654ce3c8bd4e5fbc5ab34e3213c7427
JackDownload <kismocsy@gmail.com>
2020-02-09 09:56:43 +0100

    Update cms.js

--- cms/netlify/cms.js
+++ cms/netlify/cms.js
@@ -1,12 +1,15 @@
 import Post from './post'
 import Category from './category'
 import LifeCycleHooks from './hooks/lifeCycle'
+import Recept from './recept'
+
 class CMS {
   constructor(axios = false) {
     this.name = 'Netlify'
     this.slug = 'netlify'
     this.post = new Post(axios)
     this.category = new Category(axios)
+    this.recept = new Recept(axios)
     this.lifeCycleHooks = LifeCycleHooks
   }
 }
