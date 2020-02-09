6c234217fd6a62247b890dca8e763f60bb09a915
JackDownload <kismocsy@gmail.com>
2020-02-09 09:54:25 +0100

    Update generate-recept-api.js

--- cms/netlify/build/generate-recept-api.js
+++ cms/netlify/build/generate-recept-api.js
@@ -6,13 +6,13 @@ const generateReceptApi = () => {
   const apiDir = `${rootDir}/static/api`
   const allFile = `${apiDir}/receptek.json`
   const metaFile = `${apiDir}/receptek-meta.json`
-  const pages = siteConfig.receptek.perPage
+  const receptek = siteConfig.receptek.perRecept
   const doneMessage = `{totalNumber} receptek generated in receptek API`
 
   createAll(contentDir, allFile, apiDir)
     .then((all) => {
-      const totalPages = createPagination(pages, all, `${apiDir}/receptek`)
-      createMeta({ totalPages: totalPages }, metaFile)
+      const totalReceptek = createPagination(receptek, all, `${apiDir}/receptek`)
+      createMeta({ totalReceptek: totalReceptek }, metaFile)
       console.log(doneMessage.replace('{totalNumber}', all.length))
     })
     .catch((err) => {
